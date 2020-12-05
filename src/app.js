61175c3ac584b49bbabd23e2c5be069d3b6e5c7b
dooking <123456ghghgh@naver.com>
2020-12-06 02:17:55 +0900

    [feat] 대여 메인 페이지 렌더링
    - todo : 구글 소셜 로그인

--- src/app.js
+++ src/app.js
@@ -1,32 +1,32 @@
-require("dotenv").config();
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const { sequelize } = require("./models/index");
-const indexRouter = require("./routes/index");
+require('dotenv').config();
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const { sequelize } = require('./models/index');
+const indexRouter = require('./routes/index');
 const port = process.env.PORT;
 const app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 sequelize
   .sync({ alter: false })
-  .then(() => console.log("DB 연결 성공!"))
+  .then(() => console.log('DB 연결 성공!'))
   .catch((err) => {
     console.error(err);
   });
 
-app.use("/", indexRouter);
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -37,11 +37,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 app.listen(port, () => {
--- src/routes/reservation/index.js
+++ src/routes/reservation/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("./reservation/main", { isAuth: 3 });
 });
 
 module.exports = router;
