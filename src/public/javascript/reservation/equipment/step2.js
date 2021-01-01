939fdc7aa56ca5b35a3e218390cea50d26c1418a
dooking <123456ghghgh@naver.com>
2021-01-01 23:18:30 +0900

    [feat] step2 대여목록 불러오기

--- src/public/javascript/reservation/equipment/step2.js
+++ src/public/javascript/reservation/equipment/step2.js
@@ -1,4 +1,17 @@
-const step1El = document.querySelector("#step1")
-step1El.addEventListener('click',(event)=>{
+// step1으로 이동
+const $step1El = document.querySelector("#step1")
+$step1El.addEventListener('click',(event)=>{
     history.back()
 })
+
+// 대여 목록 불러오기
+const $reservationList = document.querySelector(".reservation-list")
+for(let i=0; i<sessionStorage.length; i++) {
+    const equipment = sessionStorage.key(i);
+    const count = sessionStorage.getItem(equipment)
+
+    const listEl = document.createElement('li')
+    listEl.textContent = equipment + ' ' + sessionStorage.getItem(equipment) + '대'
+    listEl.setAttribute("class", `${equipment}`);
+    $reservationList.appendChild(listEl)
+}
